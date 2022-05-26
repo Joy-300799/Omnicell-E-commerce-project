429903de2163fc7cc8c2d56eb65b4d77f0ec4a1f
Joy-300799 <joy.arya7381@gmail.com>
2022-05-26 10:17:05 +0530

    Cart UI developed & worked on some integration

--- E-commerce/src/app/cart/cart.component.ts
+++ E-commerce/src/app/cart/cart.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cardPay: boolean = false;
+  upiPay: boolean = false;
+  codPay: boolean = false;
 
-  constructor() { }
+  checkout: boolean = false;
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  cardPayment(){
+    this.cardPay = !this.cardPay;
+    this.upiPay = false;
+    this.codPay = false;
   }
 
+  upiPayment(){
+    this.cardPay = false;
+    this.upiPay = !this.upiPay;
+    this.codPay = false;
+  }
+
+  codPayment(){
+    this.cardPay = false;
+    this.upiPay = false;
+    this.codPay = !this.codPay;
+  }
+
+  toggleCheckout(){
+    this.checkout = !this.checkout;
+  }
 }
